6eda3cfc43353bb617a625cd728af9d5f3e9d252
Максим Касакин <loliallen@MacBook-Air-Maksim.local>
2023-08-20 19:33:22 +0300

    update lol

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,34 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            borderRadius: {
+                sm: "1.25rem",
+                md: "2.185rem",
+                lg: "3.125rem",
+            },
+            colors: {
+                "blue-2": "#263146",
+                "blue-4": "#AEB9D2",
+                "blue-5": "#D6E1FA",
+                "blue-6": "#4C5576",
+                "blue-7": "#22304D",
+                "accent-green": "#11F4D1",
+            },
+            fontSize: {
+                15: "0.9375rem",
+            },
+            height: {
+                15: "3.75rem",
+            },
+        },
     },
-  },
-  plugins: [],
-}
-export default config
+    plugins: [require("tailwind-scrollbar")],
+};
+export default config;
