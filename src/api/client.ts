30e2ad5f03d69aaac5a2ae891a255b76e6e5e834
Максим Касакин <loliallen@MacBook-Air-Maksim.local>
2023-08-25 11:43:45 +0300

    wtf with env vars

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const client = axios.create({
-  baseURL: process.env.NEXT_API_URL || "http://localhost:3000",
+  baseURL: process.env.NEXT_API_URL || "",
 });
 client.interceptors.request.use((config) => {
   if (localStorage.getItem("token")) {
