daae725f4249d7ff59df97b1f290ac43dc718c94
loliallen <maxim.kasakin@yahoo.com>
2023-10-02 05:06:43 +0300

    added signature page

--- src/app/api/wishes/route.ts
+++ src/app/api/wishes/route.ts
@@ -15,61 +15,10 @@ type Body = {
 
 export const POST = async (req: Request) => {
     const data: Body = await req.json();
-    const response = await axios.post(
-        process.env["BOLDSIGN_API_URL"]!,
-        {
-            roles: [
-                {
-                    roleIndex: 1,
-                    signerName: data.name,
-                    signerEmail: data.email,
-                    existingFormFields: [
-                        {
-                            id: "DonationAmount",
-                            value: Number(data.amount).toLocaleString(),
-                        },
-                        {
-                            id: "EndorserEmail",
-                            value: data.endorser,
-                        },
-                        {
-                            id: "WishNo1",
-                            value: data.wishes[1],
-                        },
-                        {
-                            id: "WishNo2",
-                            value: data.wishes[2],
-                        },
-                        {
-                            id: "WishNo3",
-                            value: data.wishes[3],
-                        },
-                    ],
-                },
-            ],
-            // "roleRemovalIndices": [1,2] Removes the roles present in the template with their indices given in this property.
-        },
-        {
-            params: {
-                templateId: process.env["BOLDSIGN_TEMPLATE_ID"],
-            },
-            headers: {
-                Accept: "application/json",
-                "X-API-KEY": process.env["BOLDSIGN_API_KEY"],
-                "Content-Type":
-                    "application/json;odata=minimal;odata.streaming=true",
-            },
-        },
-    );
-    console.log(response);
 
-    const contractResponse = await axios.post(
-        "contract/create",
-        response.data,
-        {
-            baseURL: "https://api.aga.live",
-        },
-    );
+    const contractResponse = await axios.post("contract/create", data, {
+        baseURL: "https://api.aga.live",
+    });
 
     // console.log(contractResponse);
     // res.send(contractResponse.data);
