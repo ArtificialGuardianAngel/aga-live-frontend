6781d4a26fecc979b274a0b83c2699c32b78b729
loliallen <maxim.kasakin@yahoo.com>
2023-11-23 09:02:02 +0300

    added gist related docs;

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -3,10 +3,13 @@ import { readFileSync } from "fs";
 import { DocsRenderer } from "./renderer";
 // import { Light } from "react-syntax-highlighter";
 
-const DocsPage = () => {
-    const filePath = join(process.cwd(), "public", "docs.md");
-    const fileContent = readFileSync(filePath, 'utf-8');
+const DocsPage = async () => {
+    const response = await fetch(
+        "https://gist.githubusercontent.com/loliallen/3b75bddf169c615eef12c1b87a17abd8/raw",
+    );
+
+    const text = await response.text();
     // return <div dangerouslySetInnerHTML={{ __html: marked(fileContent) }} />;
-    return <DocsRenderer content={fileContent} />;
+    return <DocsRenderer content={text} />;
 };
 export default DocsPage;
