b5c9048c5a9d7c2c82d41c02de22f1a4668665f1
loliallen <maxim.kasakin@yahoo.com>
2023-11-25 08:08:04 +0300

    update url

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -2,7 +2,7 @@ import { DocsRenderer } from "./renderer";
 
 const DocsPage = async () => {
     const response = await fetch(
-        "https://gist.githubusercontent.com/loliallen/3b75bddf169c615eef12c1b87a17abd8/raw",
+        "https://gist.githubusercontent.com/loliallen/3b75bddf169c615eef12c1b87a17abd8/raw/821fb364170d77f881105d9331e17dc4d087c5ee/docs.md",
         {
             cache: "no-cache",
         },
